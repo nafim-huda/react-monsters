a1b9c1def63d1d6f9da31f70f526e8c382062e05
nafim-huda <nhuda@protonmail.com>
2023-03-26 17:36:37 -0400

    converted class components to functional components

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,66 +1,70 @@
-import { Component } from 'react'
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 // Component - self-contained JS function that returns a visual representation of HTML
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: '',
-    }
+/*
+Pure Function - the scope of the function's vars is limited to props passed/always returns
+same result AND contains no side effect(effect outside of the scope of the function)
+ex.)
+  const functB = (a,b) => {
+    return a * b;
   }
-  // runs the first time a component gets placed onto the DOM
-  // a component will only remount whenever there is a new instance
-  // of the component placed onto the DOM after it had been unmounted
-  componentDidMount() {
+vs.
+Impure Function - the scope of the function is not limited to props passed/body/modifies
+something outside of the scope of the function
+ex.)
+let c = 3
+const funcB = (a,b) => {
+  c = a * b;
+  return a * b;
+}
+
+Side Effects - modification of data outside of the scope of the function i.e.) useEffect() hook
+
+*/ 
+const App = () => {
+  // useState() = lets us encapsulate local state in a functional component 
+  // React is intelligent enough to understand that passing in identical state values
+  // into a useState() call -> no re-render will occur
+  const [searchField, setSearchField] = useState('') // returns array [value, setValue()]
+  const [monsters, setMonsters] = useState([])
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters)
+
+  console.log('render');
+
+  // we cannot do fetch() -> .then(users) => setMonsters(users) because it will fail
+  // equality check(the users array that we fetch from a third party is different 
+  // from the array we saved in memory) -> solution? useEffect()
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => 
-      // this.setState() here will re-render the component -> 
-      // calling render() once again 
-        this.setState(() => {
-            return {monsters : users }
-          }))
-    }
-  /* 
-    this.setState((state, props) => {
-      // returns an object that will shallow merge against state
-      // note that the state variable in the argument is equal to the local state
-    },
-    *** Optional Callback ***
-      () => {
-        // runs only AFTER all state changes have been applied
-      }
-    )
-  */
-   /* 
-      <div>, <h1>, <input> are HTML Look-Alike tags but in reality 
-      they are React HTML Components 
-   */
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  }, []); 
 
-  onSearchChange = (event) => { 
-    // Best Practice: Array immutability -> create a new array when needed rather than 
-    // modifying existing array 
-      const searchField = event.target.value.toLocaleLowerCase();
-      this.setState(() => {
-        return { searchField } // shorthand for matching key and values with same name
-      });
-    }; 
-
-  render() {
-    const { monsters, searchField } = this.state
-    const { onSearchChange } = this
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField)
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField)
     })
-      return (
+    setFilteredMonsters(newFilteredMonsters)
+  }, [monsters, searchField])
+
+  const onSearchChange = (event) => { 
+      // Best Practice: Array immutability -> create a new array when needed rather than 
+      // modifying existing array 
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+        setSearchField(searchFieldString)
+      };
+      
+   return (
         // In general, we only want one parent/top-level component in our return () 
         // in this return the parent component is <div className="App"> </div>
         <div className="App">
           <h1 className='app-title'>Monsters Rolodex</h1>
+          
           <SearchBox 
             className='monsters-search-box'
             onChangeHandler={onSearchChange} 
@@ -70,6 +74,5 @@ class App extends Component {
         </div>
       );
     }
-  }
 
 export default App;
--- monsters-rolodex/src/components/search-box/search-box.component.jsx
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -1,20 +1,14 @@
-import { Component } from "react";
 
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-    render() {   
-        return (
-            <input 
-            className={`search-box ${this.props.className}`}
-            type='search'
-            placeholder={this.props.placeholder}
-            // onChange is a callback that is called when there is an update to our input
-            // we want to bind an onChange handler for dealing with user input 
-            onChange={this.props.onChangeHandler}
-           />
-        )
-    }
-}
-
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+    <input
+        className={`search-box ${className}`}
+        type='search'
+        placeholder={placeholder}
+        // onChange is a callback that is called when there is an update to our input
+        // we want to bind an onChange handler for dealing with user input 
+        onChange={onChangeHandler}
+    />
+)
 export default SearchBox
